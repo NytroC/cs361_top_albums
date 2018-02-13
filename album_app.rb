94a3a651f93fb056b0b7d1c47c3dc8606c88c5e1
Brad Cook <cookbrad@oregonstate.edu>
2018-02-13 07:47:17 -0800

    Add sorting

--- album_app.rb
+++ album_app.rb
@@ -3,9 +3,23 @@ require 'erb'
 require_relative 'parse_album_list.rb'
 
 class AlbumApp < Sinatra::Base
+ 
+  # albums.sort_by {|_key, value| _key}.to_h
 
   get '/' do
   	@album_list = parseAlbums
+   	@template = File.read('./index.erb')
+   ERB.new(@template).result( binding )  
+  end
+
+  get '/title' do
+  	@album_list = parseAlbums.sort_by {|_key, value| _key}.to_h
+   	@template = File.read('./index.erb')
+   ERB.new(@template).result( binding )  
+  end
+
+  get '/year' do
+  	@album_list = parseAlbums.sort_by {|_key, value| value}.to_h
    	@template = File.read('./index.erb')
    ERB.new(@template).result( binding )  
   end
