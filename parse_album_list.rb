12ed3ddb00653c4c62cc84d4d4b0140e4be208e4
Brad Cook <cookbrad@oregonstate.edu>
2018-02-14 14:04:55 -0800

    Move sorting to parse_album

--- parse_album_list.rb
+++ parse_album_list.rb
@@ -1,10 +1,18 @@
 require 'csv'
 
-def parseAlbums
+def parseAlbums sorted_by
 	arr_of_arrs = CSV.read("top_100_albums.txt")
 	albums = {}
 	arr_of_arrs.each do |arr|
 		albums[arr[0]] = arr[1]
 	end
+
+	if sorted_by == 'title'
+		albums.sort_by {|_key, value| _key}.to_h
+
+	elsif sorted_by == 'year'
+		albums.sort_by {|_key, value| value}.to_h
+	end
+
 	albums
 end
